e5a16ce1d77c5b11b694f38ad2e2791f152925af
Loki Verloren <stalker.loki@protonmail.ch>
2021-07-12 21:04:17 +0300

    should be the complete map step for flex

--- canvas/flex.go
+++ canvas/flex.go
@@ -1,6 +1,10 @@
 package canvas
 
-import "github.com/cybriq/giocore/op"
+import (
+	"image"
+	
+	"github.com/cybriq/giocore/op"
+)
 
 type Flex struct {
 	*Widget
@@ -24,29 +28,10 @@ type FlexChild struct {
 	*Widget
 	weight float32
 	flex   bool
-
+	
 	// Scratch space.
 	call op.CallOp
-	dims Lim
-}
-
-// FlexMap computes the dimensions of a Flex
-func FlexMap(w *Widget) (d Dims) {
-	fl, ok := w.State.(*Flex)
-	if !ok {
-		return
-	}
-	_ = fl
-	return Dims{}
-}
-
-func FlexPaint(w *Widget) {
-	fl, ok := w.State.(*Flex)
-	if !ok {
-		return
-	}
-	// do the paint thing
-	_ = fl
+	dims Dims
 }
 
 func HFlex() *Flex {
@@ -71,7 +56,7 @@ func VFlex() *Flex {
 	wd.State = fl
 	fl.Widget = wd
 	return fl
-
+	
 }
 
 // alignment setters
@@ -97,14 +82,6 @@ func (f *Flex) AlignMiddle() (out *Flex) {
 // AlignBaseline sets alignment for layout from Baseline
 func (f *Flex) AlignBaseline() (out *Flex) {
 	f.Alignment = Baseline
-	return f
-}
-
-// Axis setters
-
-// Vertical sets axis to vertical, otherwise it is horizontal
-func (f *Flex) Vertical() (out *Flex) {
-	f.Axis = Vertical
 	return f
 }
 
@@ -146,20 +123,155 @@ func (f *Flex) SpaceEvenly() (out *Flex) {
 	return f
 }
 
-// Rigid inserts a rigid widget into the flex
-func (f *Flex) Rigid(w *Widget) (out *Flex) {
-	f.FlexChildren = append(f.FlexChildren, FlexChild{
-		Widget: w,
-	})
+// Rigid inserts rigid widgets into the flex
+func (f *Flex) Rigid(w ...*Widget) (out *Flex) {
+	for i := range w {
+		f.FlexChildren = append(f.FlexChildren, FlexChild{
+			Widget: w[i],
+		})
+	}
 	return f
 }
 
-// Flexed inserts a flexed widget into the flex
-func (f *Flex) Flexed(wgt float32, w *Widget) (out *Flex) {
-	f.FlexChildren = append(f.FlexChildren, FlexChild{
-		Widget: w,
-		weight: wgt,
-		flex:   true,
-	})
+// Flexed inserts flexed widgets into the flex
+func (f *Flex) Flexed(wgt float32, w ...*Widget) (out *Flex) {
+	for i := range w {
+		f.FlexChildren = append(f.FlexChildren, FlexChild{
+			Widget: w[i],
+			weight: wgt,
+			flex:   true,
+		})
+	}
 	return f
+}
+
+// FlexMap computes the dimensions of a Flex
+func FlexMap(w *Widget) (d Dims) {
+	fl, ok := w.State.(*Flex)
+	if !ok {
+		return
+	}
+	size := 0
+	cs := fl.Ctx.Lim
+	mainMin, mainMax := fl.Axis.MainConstraint(cs)
+	crossMin, crossMax := fl.Axis.CrossConstraint(cs)
+	remaining := mainMax
+	var totalWeight float32
+	for _, i := range fl.FlexChildren {
+		if i.flex {
+			totalWeight += i.weight
+			continue
+		}
+		fl.Ctx.Lim = fl.Axis.
+			Constraints(0, remaining, crossMin, crossMax)
+		dm := i.Map()
+		sz := fl.Axis.Convert(dm.Size).X
+		size += sz
+		remaining -= sz
+		if remaining < 0 {
+			remaining = 0
+		}
+		i.dims = *dm
+	}
+	if w := fl.WeightSum; w != 0 {
+		totalWeight = w
+	}
+	// fraction is the rounding error from a flex weighting.
+	var fraction float32
+	flexTotal := remaining
+	// Lay out flexed children.
+	for i, child := range fl.FlexChildren {
+		if !child.flex {
+			continue
+		}
+		var flexSize int
+		if remaining > 0 && totalWeight > 0 {
+			// Apply weight and add any leftover fraction from a
+			// previous flexed.
+			childSize := float32(flexTotal) * child.weight / totalWeight
+			flexSize = int(childSize + fraction + .5)
+			fraction = childSize - float32(flexSize)
+			if flexSize > remaining {
+				flexSize = remaining
+			}
+		}
+		fl.Ctx.Lim = fl.Axis.
+			Constraints(flexSize, flexSize, crossMin, crossMax)
+		dm := child.Map()
+		sz := fl.Axis.Convert(dm.Size).X
+		size += sz
+		remaining -= sz
+		if remaining < 0 {
+			remaining = 0
+		}
+		fl.FlexChildren[i].dims = *dm
+	}
+	var maxCross int
+	var maxBaseline int
+	for _, child := range fl.FlexChildren {
+		if c := fl.Axis.Convert(child.dims.Size).Y; c > maxCross {
+			maxCross = c
+		}
+		if b := child.dims.Size.Y - child.dims.Baseline; b > maxBaseline {
+			maxBaseline = b
+		}
+	}
+	var space int
+	if mainMin > size {
+		space = mainMin - size
+	}
+	var mainSize int
+	switch fl.Spacing {
+	case SpaceSides:
+		mainSize += space / 2
+	case SpaceStart:
+		mainSize += space
+	case SpaceEvenly:
+		mainSize += space / (1 + len(fl.FlexChildren))
+	case SpaceAround:
+		if len(fl.FlexChildren) > 0 {
+			mainSize += space / (len(fl.FlexChildren) * 2)
+		}
+	}
+	for i, child := range fl.FlexChildren {
+		dm := child.dims
+		mainSize += fl.Axis.Convert(dm.Size).X
+		if i < len(fl.FlexChildren)-1 {
+			switch fl.Spacing {
+			case SpaceEvenly:
+				mainSize += space / (1 + len(fl.FlexChildren))
+			case SpaceAround:
+				if len(fl.FlexChildren) > 0 {
+					mainSize += space / len(fl.FlexChildren)
+				}
+			case SpaceBetween:
+				if len(fl.FlexChildren) > 1 {
+					mainSize += space / (len(fl.FlexChildren) - 1)
+				}
+			}
+		}
+	}
+	switch fl.Spacing {
+	case SpaceSides:
+		mainSize += space / 2
+	case SpaceEnd:
+		mainSize += space
+	case SpaceEvenly:
+		mainSize += space / (1 + len(fl.FlexChildren))
+	case SpaceAround:
+		if len(fl.FlexChildren) > 0 {
+			mainSize += space / (len(fl.FlexChildren) * 2)
+		}
+	}
+	sz := fl.Axis.Convert(image.Pt(mainSize, maxCross))
+	return Dims{Size: sz, Baseline: sz.Y - maxBaseline}
+}
+
+func FlexPaint(w *Widget) {
+	fl, ok := w.State.(*Flex)
+	if !ok {
+		return
+	}
+	// do the paint thing
+	_ = fl
 }
